be356de0763d5773fec21d8cef03396510898ae2
Isaac Henderson <theisaachendo@gmail.com>
2024-10-24 16:19:01 -0400

    removed extra import

--- screens/artist/ArtistPostsPage.js
+++ screens/artist/ArtistPostsPage.js
@@ -5,7 +5,6 @@ import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import axios from 'axios';
 import { BASEURL } from '../../assets/constants';
 import ProfilePictureButton from '../../assets/components/ProfilePictureButton'; // Import ProfilePictureButton
-import ProfilePictureButton from '../../assets/components/ProfilePictureButton';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { logoutUser } from '../../redux/actions';
 
