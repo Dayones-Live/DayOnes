fdc364df1cdb840ad85a654dd9e796854a5446a5
Jesse <jessejento@gmail.com>
2024-10-29 15:55:19 -0400

    Changed Fan homepage

--- screens/fan/FHomePage.js
+++ screens/fan/FHomePage.js
@@ -1,93 +1,89 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text, StyleSheet, Switch, Alert, FlatList, TouchableOpacity, Image, SafeAreaView } from 'react-native'; // Added SafeAreaView
+import { View, Text, StyleSheet, Alert, FlatList, TouchableOpacity, Image, SafeAreaView, Modal, ActivityIndicator } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Geolocation from '@react-native-community/geolocation';
 import { useSelector, useDispatch } from 'react-redux';
-import { useFocusEffect } from '@react-navigation/native';
 import { setInvitesEnabled } from '../../assets/redux/actions';
 import { BASEURL } from '../../assets/constants';
-import useSetupNotificationsAndLocation from '../../assets/hooks/useSetupNotificationsAndLocation';
+import ProfilePictureButton from '../../assets/components/ProfilePictureButton';
 
-const FHomePage = () => {
+const FHomePage = ({ navigation }) => {
   const dispatch = useDispatch();
   const invitesFromRedux = useSelector((state) => state.invitesEnabled);
   const accessToken = useSelector((state) => state.accessToken);
 
   const [isInviteEnabled, setIsInviteEnabled] = useState(invitesFromRedux);
   const [invites, setInvites] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [countdown, setCountdown] = useState(60);
+  const [intervalId, setIntervalId] = useState(null);
 
   useEffect(() => {
-    setIsInviteEnabled(invitesFromRedux); // Sync local state with Redux state
+    setIsInviteEnabled(invitesFromRedux);
   }, [invitesFromRedux]);
 
-  useSetupNotificationsAndLocation();
+  const toggleInviteAndFetch = () => {
+    setIsLoading(true);
+    setCountdown(60);
+    dispatch(setInvitesEnabled(true));
+    setIsInviteEnabled(true);
 
-  // Refetch invites whenever the page is focused
-  useFocusEffect(
-    useCallback(() => {
-      if (isInviteEnabled) {
-        fetchInvites();
-      }
-    }, [isInviteEnabled])
-  );
-
-  const toggleInvite = async (value) => {
-    setIsInviteEnabled(value); // Update local state
-    dispatch(setInvitesEnabled(value)); // Dispatch to Redux
-    console.log('Toggling invites:', value);
-
-    // Fetch current location and make API call
     Geolocation.getCurrentPosition(
       async (position) => {
         const { latitude, longitude } = position.coords;
-        console.log('User location:', { latitude, longitude });
-
         try {
-          const response = await fetch(`${BASEURL}/api/v1/user/update-notification-status`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${accessToken}`,
-            },
-            body: JSON.stringify({
-              longitude: longitude.toString(),
-              latitude: latitude.toString(),
-              notificationsEnabled: value,
-            }),
-          });
-
-          const responseData = await response.json();
-          console.log('Update invite status response:', responseData);
-
-          if (response.ok) {
-            // Automatically fetch invites if enabled
-            if (value) {
-              fetchInvites();
-            }
-          } else {
-            console.error(`Failed to update invite status. Status: ${response.status}`);
-          }
+          await updateNotificationStatus(latitude, longitude, true);
+          startInvitePolling();
         } catch (error) {
-          console.error('Error updating invite status:', error);
           Alert.alert('Error', 'Failed to update invite status');
         }
       },
-      (error) => {
-        console.error('Failed to get location:', error);
-        Alert.alert('Error', 'Failed to get location');
-      },
+      (error) => Alert.alert('Error', 'Failed to get location'),
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     );
   };
 
-  const fetchInvites = async () => {
-    if (!isInviteEnabled) {
-      Alert.alert('Please Enable Invites', 'Enable invites to fetch invites.');
-      return;
-    }
-
+  const updateNotificationStatus = async (latitude, longitude, notificationsEnabled) => {
     try {
-      console.log('Fetching invites from API...');
+      await fetch(`${BASEURL}/api/v1/user/update-notification-status`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          longitude: longitude.toString(),
+          latitude: latitude.toString(),
+          notificationsEnabled,
+        }),
+      });
+    } catch (error) {
+      throw new Error('Failed to update notification status');
+    }
+  };
+
+  const startInvitePolling = () => {
+    const id = setInterval(() => {
+      fetchInvites();
+      setCountdown((prevCountdown) => {
+        if (prevCountdown <= 1) {
+          clearInterval(id);
+          setIsLoading(false);
+        }
+        return prevCountdown - 1;
+      });
+    }, 1000);
+    setIntervalId(id);
+  };
+
+  const handleCancel = () => {
+    if (intervalId) clearInterval(intervalId);
+    setIsLoading(false);
+    setCountdown(60);
+  };
+
+  const fetchInvites = async () => {
+    try {
       const response = await fetch(`${BASEURL}/api/v1/invites`, {
         method: 'GET',
         headers: {
@@ -95,26 +91,24 @@ const FHomePage = () => {
           Authorization: `Bearer ${accessToken}`,
         },
       });
-
-      const responseData = await response.json();
-      console.log('Invites data:', responseData);
-
       if (response.ok) {
-        // Filter only invites with status "PENDING"
+        const responseData = await response.json();
         const pendingInvites = responseData.data.filter((invite) => invite.status === 'PENDING');
+
         setInvites(pendingInvites);
-      } else {
-        console.error(`Failed to fetch invites. Status: ${response.status}`);
+
+        if (pendingInvites.length > 0) {
+          handleCancel();
+        }
       }
     } catch (error) {
-      console.error('Error fetching invites:', error);
       Alert.alert('Error', 'Failed to fetch invites.');
     }
   };
 
   const handleConfirmInvite = async (inviteId) => {
     try {
-      const response = await fetch(`${BASEURL}/api/v1/invites/${inviteId}`, {
+      await fetch(`${BASEURL}/api/v1/invites/${inviteId}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
@@ -122,22 +116,16 @@ const FHomePage = () => {
         },
         body: JSON.stringify({ status: 'ACCEPTED' }),
       });
-
-      if (response.ok) {
-        Alert.alert('Success', 'Invite confirmed.');
-        fetchInvites();
-      } else {
-        throw new Error(`Failed to confirm invite. Status: ${response.status}`);
-      }
+      fetchInvites();
+      Alert.alert('Success', 'Invite confirmed.');
     } catch (error) {
-      console.error('Error confirming invite:', error);
       Alert.alert('Error', 'Failed to confirm invite.');
     }
   };
 
   const handleDenyInvite = async (inviteId) => {
     try {
-      const response = await fetch(`${BASEURL}/api/v1/invites/${inviteId}`, {
+      await fetch(`${BASEURL}/api/v1/invites/${inviteId}`, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
@@ -145,106 +133,68 @@ const FHomePage = () => {
         },
         body: JSON.stringify({ status: 'REJECTED' }),
       });
-
-      if (response.ok) {
-        Alert.alert('Success', 'Invite denied.');
-        fetchInvites();
-      } else {
-        throw new Error(`Failed to deny invite. Status: ${response.status}`);
-      }
+      fetchInvites();
+      Alert.alert('Success', 'Invite denied.');
     } catch (error) {
-      console.error('Error denying invite:', error);
       Alert.alert('Error', 'Failed to deny invite.');
     }
   };
 
-  const renderInviteItem = ({ item }) => {
-    console.log("Invite Data:", item); // Log out all data for each invite
-
-    // Format date to show time first before date
-    const validTillDate = new Date(item.valid_till);
-    const formattedDate = `${validTillDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} ${validTillDate.toLocaleDateString()}`;
-
-    return (
-      <View style={styles.inviteItem}>
-        <View style={styles.userInfoContainer}>
-          <Image source={{ uri: item.user.avatar_url }} style={styles.avatar} />
-          <Text style={styles.userName}>{item.user.full_name}</Text>
-        </View>
-
-        {/* Invite Validity */}
-        <Text style={styles.inviteText}>Invite valid until: {formattedDate}</Text>
-
-        {/* Confirm and Deny Buttons */}
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={[styles.inviteButton, styles.confirmButton]}
-            onPress={() => handleConfirmInvite(item.id)}
-          >
-            <Text style={styles.buttonText}>Confirm</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.inviteButton, styles.denyButton]}
-            onPress={() => handleDenyInvite(item.id)}
-          >
-            <Text style={styles.buttonText}>Deny</Text>
-          </TouchableOpacity>
-        </View>
+  const renderInviteItem = ({ item }) => (
+    <View style={styles.inviteItem}>
+      <View style={styles.userInfoContainer}>
+        <Image source={{ uri: item.user.avatar_url }} style={styles.avatar} />
+        <Text style={styles.userName}>{item.user.full_name}</Text>
       </View>
-    );
-  };
+      <Text style={styles.inviteText}>Invite valid until: {new Date(item.valid_till).toLocaleString()}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.inviteButton, styles.confirmButton]} onPress={() => handleConfirmInvite(item.id)}>
+          <Text style={styles.buttonText}>Confirm</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.inviteButton, styles.denyButton]} onPress={() => handleDenyInvite(item.id)}>
+          <Text style={styles.buttonText}>Deny</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <LinearGradient colors={['#00ffcc', '#0099cc']} style={styles.header}>
-        <Text style={styles.headerText}>DayOnes Invites</Text>
-      </LinearGradient>
+      <ProfilePictureButton navigation={navigation} />
 
       <FlatList
         data={invites}
         keyExtractor={(item) => item.id}
         renderItem={renderInviteItem}
         ListEmptyComponent={<Text style={styles.noInviteText}>No Invites Available</Text>}
+        contentContainerStyle={{ paddingTop: 65 }} // Added padding to push invites down
       />
 
-      {/* Controls to toggle invites */}
       <View style={styles.controlsContainer}>
-        <View style={styles.switchContainer}>
-          <Text style={styles.switchText}>
-            {isInviteEnabled ? 'Invites Enabled' : 'Invites Disabled'}
-          </Text>
-          <Switch
-            value={isInviteEnabled}
-            onValueChange={toggleInvite}
-            trackColor={{ false: '#767577', true: '#81b0ff' }}
-            thumbColor={isInviteEnabled ? '#f5dd4b' : '#f4f3f4'}
-          />
-        </View>
-
-        <TouchableOpacity style={styles.fetchButton} onPress={fetchInvites}>
-          <Text style={styles.buttonText}>Get Invites</Text>
+        <TouchableOpacity style={styles.fetchButton} onPress={toggleInviteAndFetch}>
+          <LinearGradient colors={['#00E5FF', '#D500F9']} style={styles.fetchButtonGradient}>
+            <Text style={styles.buttonText}>Get Invites</Text>
+          </LinearGradient>
         </TouchableOpacity>
       </View>
+
+      <Modal visible={isLoading} transparent={true} animationType="fade">
+        <View style={styles.modalBackground}>
+          <ActivityIndicator size="large" color="#D500F9" />
+          <Text style={styles.countdownText}>Checking invites... {countdown}s</Text>
+          <TouchableOpacity style={styles.cancelButton} onPress={handleCancel}>
+            <Text style={styles.cancelButtonText}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
-
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0c002b',
-  },
-  header: {
-    padding: 20,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    alignItems: 'center',
-  },
-  headerText: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold',
+    backgroundColor: '#000',
   },
   inviteItem: {
     backgroundColor: '#222',
@@ -299,33 +249,44 @@ const styles = StyleSheet.create({
   controlsContainer: {
     padding: 20,
     alignItems: 'center',
-    borderTopWidth: 1,
-    borderTopColor: '#333',
-  },
-  switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '80%',
-    marginBottom: 10,
-  },
-  switchText: {
-    fontSize: 18,
-    color: '#fff',
-    fontWeight: 'bold',
   },
   fetchButton: {
-    backgroundColor: '#1e90ff',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    width: '100%',
     borderRadius: 10,
+    overflow: 'hidden',
+  },
+  fetchButtonGradient: {
+    paddingVertical: 10,
     alignItems: 'center',
+    borderRadius: 10,
   },
   noInviteText: {
     textAlign: 'center',
     color: '#fff',
     fontSize: 16,
     padding: 20,
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  countdownText: {
+    color: '#fff',
+    fontSize: 16,
+    marginTop: 10,
+  },
+  cancelButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#D500F9',
+    borderRadius: 10,
+  },
+  cancelButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
