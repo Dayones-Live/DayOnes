9c1eb2c32ca68254a1cc1d6c38b761c4260a529c
Jesse <jessejento@gmail.com>
2024-11-01 12:04:09 -0400

    red

--- screens/PermissionsScreen.js
+++ screens/PermissionsScreen.js
@@ -221,7 +221,7 @@ const PermissionItem = ({ icon, title, enabled, onPress }) => (
       onPress={onPress}
       style={[
         styles.permissionButton,
-        { backgroundColor: enabled ? '#00E5FF' : '#D500F9' },
+        { backgroundColor: enabled ? '#00E5FF' : 'red' },
       ]}
     >
       <Text style={styles.permissionButtonText}>
