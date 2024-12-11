4ae2e0a7a7428d22af47cac3a48f461d2446906a
Jesse <jessejento@gmail.com>
2024-12-11 16:17:37 -0500

    Edited the project name from com.dayones to com.dayoneslive

--- android/app/src/main/java/com/dayoneslive/MainActivity.kt
+++ android/app/src/main/java/com/dayoneslive/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dayones
+package com.dayoneslive
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/dayoneslive/MainApplication.kt
+++ android/app/src/main/java/com/dayoneslive/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.dayones
+package com.dayoneslive
 
 import android.app.Application
 import com.facebook.react.PackageList
