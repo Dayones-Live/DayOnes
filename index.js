1a6fa550981d9df2b732158070ec88c358a510fd
Isaac Henderson <theisaachendo@gmail.com>
2024-12-18 11:54:38 -0500

    index updated

--- index.js
+++ index.js
@@ -4,9 +4,13 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import messaging from '@react-native-firebase/messaging'; // Import messaging
 import {name as appName} from './app.json';
+
+// Handle background messages
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Background message handled:', remoteMessage);
 });
 
+// Register the app component
 AppRegistry.registerComponent(appName, () => App);
