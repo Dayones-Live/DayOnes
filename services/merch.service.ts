b5d2730f27f670d3316bed0602fa4cb36323dd80
Hevin40 <hevinmgolakiya4@gmail.com>
2026-03-21 10:01:55 +0530

    Add artist merch API functions to service layer

--- services/merch.service.ts
+++ services/merch.service.ts
@@ -43,3 +43,40 @@ export const requestReturn = async (orderId: string) => {
   const response = await axiosInstance.patch(`${MERCH_BASE}/orders/${orderId}/return`);
   return response.data?.data;
 };
+
+const STRIPE_BASE = '/api/v1/stripe';
+
+export const getArtistDrops = async () => {
+  const response = await axiosInstance.get(`${MERCH_BASE}/drops`);
+  return response.data?.data;
+};
+
+export const createMerchDrop = async (artistPostId: string) => {
+  const response = await axiosInstance.post(`${MERCH_BASE}/drops`, { artistPostId });
+  return response.data?.data;
+};
+
+export const cancelMerchDrop = async (dropId: string) => {
+  const response = await axiosInstance.delete(`${MERCH_BASE}/drops/${dropId}`);
+  return response.data;
+};
+
+export const getPayoutHistory = async () => {
+  const response = await axiosInstance.get(`${MERCH_BASE}/payouts`);
+  return response.data?.data;
+};
+
+export const getPayoutBalance = async () => {
+  const response = await axiosInstance.get(`${MERCH_BASE}/payouts/balance`);
+  return response.data?.data;
+};
+
+export const getStripeConnectStatus = async () => {
+  const response = await axiosInstance.get(`${STRIPE_BASE}/connect/status`);
+  return response.data?.data;
+};
+
+export const startStripeOnboarding = async () => {
+  const response = await axiosInstance.post(`${STRIPE_BASE}/connect/onboard`);
+  return response.data?.data;
+};
